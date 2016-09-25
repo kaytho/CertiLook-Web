6c7ed9775124fd3b56cdc8c8b3d63bd61ca3514a
James Quick <jamesquick@Jamess-MacBook-Pro-2.local>
2016-09-24 20:00:04 -0500

    sdfs

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ mongoose.connect('mongodb://localhost/certilook');
 //Initialize App
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 //Body parser config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,14 +50,18 @@ app.post("/", function(req,res){
     })
 })
 
+app.listen(port, function() {
+    console.log('CertiLook is running on http://localhost:' + port);
+});
+
 /**  app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 }); */
 
-app.set('port', (process.env.PORT || 5000));
+/** app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+}); */
 
  /**  app.listen(process.env.PORT, process.env.IP, function(){
         console.log("CertiLook is up and running")
