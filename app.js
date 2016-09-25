0136a17b5c2f31ae75e238659c71ee1641b06bd2
James Quick <jamesquick@Jamess-MacBook-Pro-2.local>
2016-09-24 19:43:48 -0500

    sdfs

--- app.js
+++ app.js
@@ -46,8 +46,13 @@ app.post("/", function(req,res){
     })
 })
 
- app.listen(3000, function () {
+/**  app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+}); */
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
  /**  app.listen(process.env.PORT, process.env.IP, function(){
